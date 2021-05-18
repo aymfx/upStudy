32dc22e14606ea6777e487415a79d074e96785af
aymfx <1095731371@qq.com>
2021-05-18 14:58:19 +0800

    init

---
+++ vue-next/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <hello-world :sex="1" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/Main.vue";
+export default {
+  setup() {},
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
